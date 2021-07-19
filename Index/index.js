d6c4aee5f05c880072001a17c2f4b85c442eff8b
Gupta <Jayesh.Gupta@igl.co.in>
2021-07-19 15:25:10 +0530

    First commit

---
+++ Index/index.js
@@ -0,0 +1,112 @@
+// const canvas = document.getElementById("canvas");
+
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+
+
+
+// window.addEventListener("resize", function(){
+//     canvas.width = window.innerWidth;
+//     canvas.height = window.innerHeight;  
+// })
+
+// const context= canvas.getContext("2d");
+
+// // c.fillRect(100,100,20,20);
+
+// // ------line----
+
+// // c.beginPath();
+// // c.moveTo(30,30);
+// // c.lineTo(60,60);
+// // c.lineTo(400,500);
+// // c.stroke();
+
+// //-----circle
+// // context.beginPath();
+// // context.arc(30, 30, 30, Math.PI / 180 * 0, Math.PI / 180 * 360, false);
+// // context.stroke();
+
+
+
+// // for(var i=0 ;i<100; i++){
+    
+
+// //     context.beginPath();
+// //     context.arc(x, y, 30, Math.PI / 180 * 0, Math.PI / 180 * 360, false);
+// //     context.stroke();
+// // }
+
+// function Circle(x,y,dx,dy,radius){
+//     this.x=x;
+//     this.y= y;
+//     this.dx=dx;
+//     this.dy=dy;
+//     this.radius=radius;
+    
+//     this.draw = function(){
+       
+//         context.beginPath();
+//         context.arc(this.x, this.y, this.radius, Math.PI / 180 * 0, Math.PI / 180 * 360, false);
+//         // context.context.fillStyle='#179282';
+        
+        
+//         context.stroke();
+
+//     };
+
+//     this.update = function(){
+//         if(this.x+this.radius>innerWidth || this.x+this.radius<0){
+//         this.dx=-this.dx;
+        
+//     }else if( this.y+this.radius>innerHeight||this.y+this.radius<0){
+//         this.dy=-this.dy;
+//     }
+//     this.y+=this.dy;
+//     this.x+=this.dx;
+
+//     this.draw(); 
+//     };
+
+    
+// }
+
+// // var circle = new Circle(20,20,2,2,20);
+
+// var carray = [];
+// console.log(carray);
+
+
+// for(var i = 0; i < 100; i++)
+// {
+
+//     var x = Math.random() * innerWidth;
+//     var y = Math.random() * innerHeight;
+//     var dx= (Math.random()-0.5) * 3;
+//     var dy= (Math.random()-0.5) * 3;
+//     var radius=5;
+    
+//     carray.push(new Circle(x,y,dx,dy,radius));
+// //     circleArray.push(i);
+// }
+
+// console.log(carray);
+
+
+
+// function animate(){
+//     requestAnimationFrame(animate);
+//     context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+//     for (var i= 0; i < carray.length; i++) {
+        
+//         carray[i].update();
+         
+//      };
+//     //  circle.update();
+ 
+     
+// }
+
+// animate();
+
